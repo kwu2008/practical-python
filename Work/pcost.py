75d5db0b61a4a2e34a0fda3c0140b6d2f4345c70
Kevin Wu <YWu2@slb.com>
2022-06-24 12:13:12 -0500

    changes

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+
+
+f = open('Data/portfolio.csv', 'rt')
+
+headers = next(f);
+
+sum = 0;
+
+for line in f:
+    row = line.split(',')
+    print(row)
+    sum += float(row[2].strip())
+
+print(f'Total cost is {sum:0.2f}')
+
+f.close()
